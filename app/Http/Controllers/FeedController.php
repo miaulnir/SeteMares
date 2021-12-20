593647f4751c4bbcb4654e28b6a871c090dca1c4
abandurko <abandurko@zuzex.com>
2021-12-20 10:18:02 +0300

    Added api-route

---
+++ app/Http/Controllers/FeedController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Feed;
+use Illuminate\Http\Request;
+
+class FeedController extends Controller
+{
+    public function list(Request $request)
+    {
+        $query = Feed::with('medias');
+        if ($request->filled('category')) {
+            $category = $request->get('category');
+            $query->where('categories', 'LIKE', "%{$category}%");
+        }
+
+        if ($request->filled('search')) {
+            $search = $request->get('search');
+            $query->where('title', 'LIKE', "%{$search}%")->orWhere('content', 'LIKE', "%{$search}%");
+        }
+
+        return $query->get();
+    }
+}
